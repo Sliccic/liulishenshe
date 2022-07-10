754aeeeb6a86380ac52aac98def44f726717df61
Sliccic <60773351+Sliccic@users.noreply.github.com>
2022-07-10 14:51:57 +0800

    Update online.js

--- components/online.js
+++ components/online.js
@@ -6,7 +6,7 @@ export default function App({ server = "sg" }) {
 
   var host;
   if(server == "正式服"){
-    host = "genshin.yuanshen.cf"
+    host = "www.mihoyou.top"
   }
   
   const { data, error } = useSWR(
