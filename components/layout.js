8cfea56417b9a32646702cbf99d596106cce2e55
Sliccic <60773351+Sliccic@users.noreply.github.com>
2022-07-10 13:40:08 +0800

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -4,14 +4,14 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-export const siteTitle = '天理服务器'
+export const siteTitle = '琉璃神社'
 
 export default function Layout({ children, home }) {
     return (
         <div className={styles.container}>
             <Head>                
                 <link rel="icon" href="/favicon.ico" />
-                <meta name="description" content="天理服务器" />
+                <meta name="description" content="琉璃神社" />
             </Head>
             <header>
 
@@ -32,7 +32,7 @@ export default function Layout({ children, home }) {
             )}
             <footer class="text-center py-3">
                 <p>
-                    <a href="https://discord.gg/RnRnYcEc7J" target="_blank" rel="sponsored">加群</a>
+                    <a href="https://jq.qq.com/?_wv=1027&k=2JZmdxRY" target="_blank" rel="sponsored">加群</a>
                 </p>
                 <p class="mt-4">
                     <a href="https://github.com/casksteven/Casks-server" target="_blank" rel="sponsored">@Casksteven | 原作者Yuki</a>
