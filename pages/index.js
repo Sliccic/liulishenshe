4eadcd10c0c63996d274ffda87aa472028039c33
Sliccic <60773351+Sliccic@users.noreply.github.com>
2022-07-11 12:38:38 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -32,21 +32,21 @@ export default function Home({ allPostsData }) {
         <div class="text-center py-3">
           
           <div class="stats bg-primary text-primary-content">
-            <Online server="天理一服" />
-            <Online server="天理二服" />
-            <Online server="天理魂服" />
+            <Online server="养成服" />
+            <Online server="指令服" />
+            <Online server="敬请期待" />
           </div>
 
         </div>
         
         <p class="text-center">
-          一服电脑：1.casks.me  手机：https://1.casks.me <br/>
-          二服电脑：2.casks.me  手机：https://2.casks.me <br/>
-          测试服电脑：soul.casks.me  手机：https://soul.casks.me <br/>
+          一服电脑：www.yuanshen.cf手机：https://www.yuanshen.cf <br/>
+          二服电脑：106.13.5.214:4433  手机：106.13.5.214:4433 <br/>
+          测试服电脑：敬请期待  手机：敬请期待 <br/>
         </p>
 
         <p class="description">
-        <a href="https://casks.me" target="_blank" rel="sponsored">网站|教程</a>
+        <a href="https://www.yuanshen.cf" target="_blank" rel="sponsored">主页</a>
         </p>
 
       </div>
