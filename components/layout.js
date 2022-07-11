8ab13b787148940e063c512dba9622c66a8f0e1d
Sliccic <60773351+Sliccic@users.noreply.github.com>
2022-07-11 23:38:09 +0800

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-export const siteTitle = '<font color=#f1b8f1>琉</font><font color=#d9b8f1>璃</font><font color=#f1ccb8>神</font><font color=#f1f1b8>社</font>'
+export const siteTitle = '<color=#f1b8f1>琉</color><color=#d9b8f1>璃</color><color=#f1ccb8>神</color><color=#f1f1b8>社</color>'
 
 export default function Layout({ children, home }) {
     return (
