2664974ee79c3c4064c646f473c137f2e93d4258
Sliccic <60773351+Sliccic@users.noreply.github.com>
2022-07-11 10:35:51 +0800

    Update online.js

--- components/online.js
+++ components/online.js
@@ -5,12 +5,12 @@ const fetcher = (url) => fetch(url).then((res) => res.json());
 export default function App({ server = "sg" }) {
 
   var host;
-  if(server == "天理一服"){
-    host = "1.casks.me"
-  } else if(server == "天理二服"){
-    host = "2.casks.me"
-  } else if(server == "天理魂服"){
-    host = "soul.casks.me"
+  if(server == "养成服"){
+    host = "www.yuanshen.ml:4433"
+  } else if(server == "指令服"){
+    host = "106.13.5.214:4433"
+  } else if(server == "即将开放"){
+    host = "www.ysycf.tk"
   }
 
   const { data, error } = useSWR(
