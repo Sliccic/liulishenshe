317ccbc08bbbe5696fa0f84a843cb31db7732545
Sliccic <60773351+Sliccic@users.noreply.github.com>
2022-07-11 12:21:42 +0800

    Add files via upload

---
+++ components/layout.js
@@ -0,0 +1,44 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from './layout.module.css'
+import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
+
+export const siteTitle = '天理服务器'
+
+export default function Layout({ children, home }) {
+    return (
+        <div className={styles.container}>
+            <Head>                
+                <link rel="icon" href="/favicon.ico" />
+                <meta name="description" content="天理服务器" />
+            </Head>
+            <header>
+
+            </header>
+            {children}
+            {home ? (
+                <>
+
+                </>
+            ) : (
+                <>
+                    <h3 class="text-center">
+                        <Link href="/">
+                            <button class="btn btn-primary">Back to Home</button>
+                        </Link>
+                    </h3>
+                </>
+            )}
+            <footer class="text-center py-3">
+                <p>
+                    <a href="https://discord.gg/RnRnYcEc7J" target="_blank" rel="sponsored">加群</a>
+                </p>
+                <p class="mt-4">
+                    <a href="https://github.com/casksteven/Casks-server" target="_blank" rel="sponsored">@Casksteven | 原作者Yuki</a>
+                </p>
+
+            </footer>
+        </div>
+    )
+}
