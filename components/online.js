41ff84c5d2d2da7ebb6484a2d9a6c3f79b4436e8
Sliccic <60773351+Sliccic@users.noreply.github.com>
2022-07-30 09:51:13 +0800

    Update online.js

--- components/online.js
+++ components/online.js
@@ -4,18 +4,17 @@ export default function App({ server = "sg" }) {
   var seraddress;
   var serName;
   if(server == "s1"){
-    serName = "3.0前瞻:"
+    serName = "3.0前瞻"
     host = "285.ysycf.ml"
-    seraddress = "3.0前瞻 电脑：https://www.123pan.com/s/PFbrVv-2PXzv提取码:llss 手机：暂不可用"
+    seraddress = "在线人数"
   } else if(server == "s2"){
-    serName = "2.8指令:"
+    serName = "2.8指令"
     host = "www.ysycf.ml"
-    seraddress = "2.8指令 电脑：106.13.5.214:4433 手机：暂不可用"
+    seraddress = "在线人数"
   } else if(server == "s3"){
-    serName = "2.8养成:"
+    serName = "2.8养成"
     host = "www.yuanshen.ml:4434"
-    seraddress = "2.8养成 电脑：www.yuanshen.ml:4434 手机：https://www.123pan.com/s/PFbrVv-hPXzv提取码:llss"
-  }
+    seraddress = "在线人数"}
   
 
 
